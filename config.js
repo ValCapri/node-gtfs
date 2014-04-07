0de919d64e3f846febbc75f08e6dd56552a2a2c4
Lionel Schinckus <valcapri@me.com>
2014-04-07 18:02:24 +0200

    Change to use TEC GTFS data courtesy of OpenOV

--- config.js
+++ config.js
@@ -3,12 +3,7 @@ module.exports = {
   , agencies: [
       /* Put agency_key names from gtfs-data-exchange.com.  
       Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com */
-      'alamedaoakland-ferry'
-    , { agency_key: 'caltrain', url: 'http://www.gtfs-data-exchange.com/agency/caltrain/latest.zip'}
-    , 'ac-transit'
-    , 'county-connection'
-    , 'san-francisco-municipal-transportation-agency'
-    , 'bay-area-rapid-transit'
-    , 'golden-gate-ferry'
+      { agency_key: 'tec', url: 'http://gtfs.ovapi.nl/tec/gtfs-tec.zip'}
+
     ]
 }
